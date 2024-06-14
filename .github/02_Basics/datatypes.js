bb17ecf25bafbd0b4f3e960aac439dec432b078a
Abdulwahab009 <103354307+Abdulwahab009@users.noreply.github.com>
2024-06-14 11:53:25 +0000

    data types

---
+++ .github/02_Basics/datatypes.js
@@ -0,0 +1,8 @@
+// here we will talk about some data type
+// javascript have few data types eg
+// 1  string , num ,bolean etc
+// 2 and very intresting one: null => which is also know as stand alone value and if you try to log it out with 
+// typeof fun it will give it as an object 
+// 3 other one is : undefine 
+// 4 other one is : symbole => usually use for uniqness.
+// 5  other one is : object => this one is very important and will talk about it later in details 
